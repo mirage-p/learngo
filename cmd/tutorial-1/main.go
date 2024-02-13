57967de860683f14728fa061c56d027e0b7f5475
Miraj Patel <mirajpatel@Mirajs-MacBook-Air.local>
2024-02-13 17:10:46 -0500

    learn go pointers

--- cmd/tutorial-1/main.go
+++ cmd/tutorial-1/main.go
@@ -10,8 +10,22 @@ func main() {
 	var notAllocatedSlice = []int{}
 	var allocatedSlice = make([]int, 0, n)
 
-	fmt.Printf("Total time without preallocation: %v\n", timeLoop(notAllocatedSlice, n))
-	fmt.Printf("Total time with preallocation: %v\n", timeLoop(allocatedSlice, n))
+	fmt.Printf("\nTotal time without preallocation: %v\n", timeLoop(notAllocatedSlice, n))
+	fmt.Printf("\nTotal time with preallocation: %v\n", timeLoop(allocatedSlice, n))
+
+	var arr1 = [5]float64{1, 2, 3, 4, 5}
+	fmt.Printf("\nThe memory location for arrary 1 is: %p", &arr1)
+	var result [5]float64 = square(&arr1)
+	fmt.Printf("\nThe result is: %v", result)
+	fmt.Printf("\nThe value is: %v", arr1)
+}
+
+func square(float *[5]float64) [5]float64 {
+	fmt.Printf("\nThe memory location of the second array is: %p", float)
+	for i := range float {
+		float[i] = float[i] * float[i]
+	}
+	return *float
 }
 
 func timeLoop(slice []int, n int) time.Duration {
